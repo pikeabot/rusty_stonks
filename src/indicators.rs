0129f059a5ca770546f2b1f5d04d276bdf2ef886
Jocelyn Chang <jrc300@gmail.com>
2023-02-15 17:57:43 -0800

    changed to return NaiveTime and fixed bugs

--- src/indicators.rs
+++ src/indicators.rs
@@ -1,67 +1,70 @@
+use chrono::NaiveDate;
 use postgres::{Client, Error, NoTls};
 use crate::mean_reversion::postgres_utils::StockData;
 
-pub fn rsi(stock_data: &[StockData]) -> Vec<f32> {
-    let n = 14.0;
-
-    // for i in 0..10 {
-    //     println!("{}", stock_data[i].close);
-    // }
-
+pub fn rsi(days: f32, stock_data: &[StockData]) -> Vec<(NaiveDate, f32)> {
+    let n= days as usize;
     // https://www.investopedia.com/terms/r/rsi.asp
     let mut init_avg_gain: Vec<f32> = Vec::new();
     let mut init_avg_loss: Vec<f32> = Vec::new();
-    for i in 14..stock_data.len() {
+    for i in n-1..stock_data.len() {
         let mut upward: f32 = 0.0;
         let mut downward: f32 = 0.0;
-        for j in i-13..i {
+        for j in i+2-n..i {
             if stock_data[j].close > stock_data[j-1].close {
                 upward = upward + stock_data[j].close;
             } else if stock_data[j].close < stock_data[j-1].close {
                 downward = downward + stock_data[j].close;
             }
         }
-        let daily_init_avg_gain = upward/n;
-        let daily_init_avg_loss = downward/n;
+        let daily_init_avg_gain = upward/days;
+        let daily_init_avg_loss = downward/days;
         init_avg_gain.push(daily_init_avg_gain);
         init_avg_loss.push(daily_init_avg_loss);
     }
-    let mut avg_gain: Vec<f32> = Vec::new();
-    let mut avg_loss: Vec<f32> = Vec::new();
+    let mut rsi: Vec<(NaiveDate, f32)> = Vec::new();
     for i in 1..init_avg_gain.len() {
-        let ag: f32 = (init_avg_gain[i-1] * 13.0 + init_avg_gain[i])/n;
-        let al:f32  = (init_avg_loss[i-1] * 13.0 + init_avg_loss[i])/n;
-        avg_gain.push(ag);
-        avg_loss.push(al);
+        let avg_gain: f32 = (init_avg_gain[i-1] * 13.0 + init_avg_gain[i])/days;
+        let avg_loss:f32  = (init_avg_loss[i-1] * 13.0 + init_avg_loss[i])/days;
+        let rsi_0: f32 = 100.0 - (100.0/(1.0+avg_gain/avg_loss));
+        let id = (stock_data[i+n-1].date, rsi_0 );
+        rsi.push(id);
     }
-
-    // for i in 0..10 {
-    //     println!("{}", avg_gain[i]);
-    // }
-
-    let mut rsi: Vec<f32> = Vec::new();
-    for i in 0..avg_gain.len() {
-        let rsi_0: f32 = 100.0 - (100.0/(1.0+avg_gain[i]/avg_loss[i]));
-        rsi.push(rsi_0);
-    }
-
     rsi
-    // for i in 0..30 {
-    //     println!("{}", rsi[i]);
-    // }
+
 }
 
-pub fn ema(days: f32, stock_data: &[StockData]) -> Vec<f32>{
+pub fn sma(days: f32, stock_data: &[StockData]) -> Vec<(NaiveDate, f32)>{
+    //https://www.investopedia.com/terms/s/sma.asp#toc-what-is-a-simple-moving-average-sma
+    let mut sma: Vec<(NaiveDate, f32)> = Vec::new();
+    let n = days as usize;
+    for i in n-1..stock_data.len() {
+        let mut total = 0.0;
+        for j in i+1-n..i {
+            total = total + stock_data[j].close;
+        }
+        sma.push(( stock_data[i].date.clone(), total/days ));
+    }
+    for i in 0..5 {
+        println!("{} {}", sma[i].0, sma[i].1);
+    }
+    sma
+}
+
+pub fn ema(days: f32, stock_data: &[StockData]) -> Vec<(NaiveDate, f32)>{
+    // https://www.investopedia.com/terms/e/ema.asp#toc-calculating-the-ema
     let smoothing = 2.0;
     let m = smoothing/(1.0 + days);
-
-    let mut ema: Vec<f32> = Vec::new();
-    ema.push(stock_data[0].close * m);
-    for i in 1..stock_data.len() {
-        ema.push(stock_data[i].close * m + ema[i-1] * (1.0 - m));
-    }
+    let n = days as usize;
+    let mut ema: Vec<(NaiveDate, f32)> = Vec::new();
+    ema.push((stock_data[0].date, stock_data[0].close * m));
     ema
 }
 
 pub fn bollinger_bands() {}
+
+pub fn standard_deviation() {}
+
+pub fn on_balance_volume() {}
+
 
