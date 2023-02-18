5abc1ba5ec92579040b86d59516b069e5426281c
Jocelyn Chang <jrc300@gmail.com>
2023-02-17 22:20:55 -0800

    fixed imports

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod algos {
+    pub mod ema_crossing;
+    pub mod mean_reversion;
+}
+
+pub mod postgres_utils;
+
+pub mod indicators;
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+use rusty_stonks::algos::ema_crossing::ema_crossing;
+fn main() -> Result<(), Box<dyn std::error::Error>>  {
+        ema_crossing()
+}
